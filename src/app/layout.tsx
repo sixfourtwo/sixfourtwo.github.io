7e3ecc2e88a3ca0b80bee057f94d17c4efe507be
Christoffer Iversen Junge <christofferju@gmail.com>
2025-10-12 21:11:17 +0200

    Test fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Source_Code_Pro } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const scp = Source_Code_Pro({
+  subsets: ["latin"]
+})
 
 export const metadata: Metadata = {
   title: "Christoffer Iversen Junge",
@@ -23,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={scp.className}>
+      <body className={scp.className}>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,16 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import PersonalInfo from "./sections/personalInfo";
 import Experience from "./sections/experience";
+import { Geist, Zain, Jacquard_12} from "next/font/google";
+
+const jacq = Jacquard_12({weight: "400", subsets: ["latin"]});
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <div>
-          <h1>Christoffer Iversen Junge</h1>
+          <h1 className={jacq.className}>Christoffer Iversen Junge</h1>
         </div>
         <PersonalInfo />
         <Experience />
