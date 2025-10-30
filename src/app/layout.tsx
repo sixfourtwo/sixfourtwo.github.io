fb36f4234abe823a80cb397b3ff95169aefaf3e9
Christoffer Junge <chjun@itm8.com>
2025-10-30 10:49:45 +0100

    Add nicer icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={scp.className}>
+      <meta name="apple-mobile-web-app-title" content="jnge" />
       <body className={scp.className}>
         {children}
       </body>
